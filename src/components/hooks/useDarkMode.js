87c08717ba60155b686538c4c7fc53f5a69e9499
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-03-12 15:17:53 -0500

    bug fixes

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -5,8 +5,11 @@ export const useDarkMode = (value) => {
     const [darkMode, setDarkMode] = useLocalStorage('darkmode', value);
     
     useEffect(() => {
-        darkMode ? document.querySelector('body').classList.add('dark-mode')
-                 : document.querySelector('body').classList.remove('dark-mode')
+        if(darkMode === true){
+            document.querySelector('body').classList.add('dark-mode')
+        } else {
+            document.querySelector('body').classList.remove('dark-mode')
+        }
     }, [darkMode])
 
     return [darkMode, setDarkMode]
